374cccd97005c0fa0c4199f7fa6443198e3a4db2
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-10-13 12:29:02 -0700

    fix api call

--- app/utils/nhl_api.py
+++ app/utils/nhl_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_nhl_player_stats(player_id):
-    url = f'https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=yearByYear'
+    url = f'https://api-web.nhle.com/v1/player/{player_id}/landing'
     response = requests.get(url)
     
     if response.status_code == 200:
