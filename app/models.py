503ced1e1bba2432f17900e59d2b9eff7160b201
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-10-13 14:08:44 -0700

    add gamelog model

--- app/models.py
+++ app/models.py
@@ -27,3 +27,22 @@ class Player(db.Model):
 
     def __repr__(self):
         return f'<Player {self.first_name} {self.last_name}>'
+    
+class GameLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.player_id'), nullable=False)
+    game_id = db.Column(db.Integer, nullable=False)
+    game_date = db.Column(db.String(20), nullable=False)
+    opponent = db.Column(db.String(50), nullable=False)
+    home_road_flag = db.Column(db.String(1), nullable=False)  # 'H' for Home, 'R' for Road
+    goals = db.Column(db.Integer, nullable=False)
+    assists = db.Column(db.Integer, nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+    shots = db.Column(db.Integer, nullable=False)
+    plus_minus = db.Column(db.Integer, nullable=False)
+    power_play_goals = db.Column(db.Integer, nullable=False)
+    pim = db.Column(db.Integer, nullable=False)  # Penalty Minutes
+    toi = db.Column(db.String(10), nullable=False)  # Time on Ice
+
+    def __repr__(self):
+        return f'<GameLog {self.player_id} - {self.game_date}>'
