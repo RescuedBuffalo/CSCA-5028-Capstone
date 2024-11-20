22e95d84bcea970779fff5d1f0ed75d03be85875
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-11-20 12:10:37 -0800

    remove dev config hardcode

--- app/run.py
+++ app/run.py
@@ -2,7 +2,7 @@ from app import create_app
 import os
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app('development')  # You can change 'development' to 'production' when deploying
+app = create_app(config_name=config_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
